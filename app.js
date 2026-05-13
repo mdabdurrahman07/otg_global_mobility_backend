4d99c9c0219f88ccde33b979585487f28b9cee02
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-05-14 02:24:09 +0600

    cors added

--- app.js
+++ app.js
@@ -11,11 +11,24 @@ import inquiryRoutes from "./src/routes/inquiryRoutes.js";
 import emailRoutes from "./src/routes/emailRoutes.js";
 
 const app = express();
-
 // Middlewares
+const allowedOrigins = [
+  "http://localhost:3000",
+  process.env.CORS_ORIGIN
+].filter(Boolean);
+// CORS
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
